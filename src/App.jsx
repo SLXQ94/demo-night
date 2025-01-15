1aefb525f0204f59b227fc077e1c2425f758db50
SLXQ94 <121443463+SLXQ94@users.noreply.github.com>
2025-01-15 20:29:23 +0530

    added basename to router

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import Videos from "./sections/Videos";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <Routes>
         {/* Landing Page */}
         <Route
